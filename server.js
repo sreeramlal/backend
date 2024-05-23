1d1802d99a7d112dc51e97abe291082e69358c31
sreeramlal <124680404+sreeramlal@users.noreply.github.com>
2024-05-23 23:22:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+// server/index.js
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const cors = require('cors');
+
+
+app.use(cors());
+
+const getStoryDetails = async (id) => {
+  const response = await axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`);
+  return response.data;
+};
+
+app.get('/api/topstories', async (req, res) => {
+  const { data: topStoryIds } = await axios.get('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty');
+  const stories = await Promise.all(topStoryIds.slice(0, 10).map(getStoryDetails));
+  res.json({ stories });
+});
+
+app.get('/api/beststories', async (req, res) => {
+  const { data: bestStoryIds } = await axios.get('https://hacker-news.firebaseio.com/v0/beststories.json?print=pretty');
+  const stories = await Promise.all(bestStoryIds.slice(0, 10).map(getStoryDetails));
+  res.json({ stories });
+});
+
+app.get('/api/newstories', async (req, res) => {
+  const { data: newStoryIds } = await axios.get('https://hacker-news.firebaseio.com/v0/newstories.json?print=pretty');
+  const stories = await Promise.all(newStoryIds.slice(0, 10).map(getStoryDetails));
+  res.json({ stories });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
